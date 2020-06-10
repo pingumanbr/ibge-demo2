f556a2e04fb74433d3012fc78d6dd97ab7cac817
pingumanbr <manoel.dantas@gmail.com>
2020-06-10 14:13:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
 
 var ip = process.env.IP || '0.0.0.0';
 
@@ -18,6 +13,16 @@ var port = process.env.PORT || 8080;
 App.listen(port, ip, function() {
  console.log('running at ' + ip + ':' + port);
  });
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
