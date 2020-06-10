a3dd64fd738318dc1d32ba309a7ab6d2834d896f
pingumanbr <manoel.dantas@gmail.com>
2020-06-10 13:39:20 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+var ip = process.env.IP || '0.0.0.0';
+
+var port = process.env.PORT || 8080;
+
+App.listen(port, ip, function() {
+ console.log('running at ' + ip + ':' + port);
+ });
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
