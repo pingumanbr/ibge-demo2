cc6ba9b05957ec28bc967eae6e568e14f4328042
pingumanbr <manoel.dantas@gmail.com>
2020-06-16 19:53:43 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,9 @@ module.exports = {
   devServer: {
     compress: true,
     disableHostCheck: true,   // That solved it
-    contentBase: path.join(__dirname, '/src/index.js'),
+    publicPath: "/",
+    contentBase: "./src",
+    hot: true
     port:8080
  },      
  module: {
