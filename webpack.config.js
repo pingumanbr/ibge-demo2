31b5ddd8a4e2c0d5717c2980d8e9a0c7a3ecbd64
pingumanbr <manoel.dantas@gmail.com>
2020-06-16 19:56:19 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     disableHostCheck: true,   // That solved it
     publicPath: "/",
     contentBase: "./src",
-    hot: true
+    hot: true,
     port:8080
  },      
  module: {
