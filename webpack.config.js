8facf049bb50295e71715a286b342024d8d8be37
pingumanbr <manoel.dantas@gmail.com>
2020-06-16 18:05:25 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
    }
   },
   {
+    test: /\.(png|gif|woff|woff2|eot|ttf|svg)$/,
+    loader: "url-loader?limit=100000"
+  },                  
+  {
    test: /\.css$/,
    loader: "style-loader!css-loader"
   }]
