7e4bdad74cad1925c51b7313d0d10d40831d2549
pingumanbr <manoel.dantas@gmail.com>
2020-06-16 19:28:59 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
   path: path.join(__dirname, 'src'),
   filename: 'bundle.js'
  },
+  devServer: {
+    compress: true,
+    disableHostCheck: true,   // That solved it
+
+ },      
  module: {
   rules: [{
    test: /.jsx?$/,
