0942850290feacb9b839bf5806021a9caedca115
pingumanbr <manoel.dantas@gmail.com>
2020-06-17 09:15:33 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -47,4 +47,10 @@ module.exports = {
   test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
   exclude: /node_modules/,
   use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+},         
+  {
+   test: /\.css$/,
+   loader: "style-loader!css-loader"
+  }]
+ }
 }
